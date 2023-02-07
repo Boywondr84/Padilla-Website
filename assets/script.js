44cba2a90d22d20b82ead1533aa205a949ee8ac0
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2023-02-06 22:59:54 -0600

    typewriter text on page load

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// Header animation
+var i = 0;
+var txt = 'Johnny Padilla';
+var speed = 200;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("intro").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+window.onload=typeWriter();
+
